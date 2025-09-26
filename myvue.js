bd0584513121f07f6a0e32fe8bc27ca1edda81fb
lanlantu <zhangxi_830@163.com>
2025-09-26 10:44:18 +0800

    提交项目

---
+++ myvue.js
@@ -0,0 +1,14 @@
+class Myvue {
+    constructor(parameters) {
+       Object.keys(parameters.data).forEach((propertyName,index)=>{
+        Object.defineProperty(this,propertyName,{
+            get(){
+                return parameters.data[propertyName]
+            },
+            set(val){
+                parameters.data[propertyName]=val
+            }
+        })
+    })
+    } 
+}       
