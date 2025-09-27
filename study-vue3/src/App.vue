570862fbdf626221633af8fa77259904af8b07ec
lanlantu <zhangxi_830@163.com>
2025-09-27 10:25:58 +0800

    添加vue3

---
+++ study-vue3/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Hello from './components/hello.vue';
+
+</script>
+
+<template>
+  <Hello></Hello>
+</template>
+
+<style scoped></style>
---
+++ study-vue3/src/components/hello.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>Hello</div>
+</template>
+
+<script setup lang="ts">
+import {ref,reactive} from'vue' 
+
+const name=ref('张三');
+const person=reactive({
+    name:'lisi',
+    age:12
+})
+
+const car=ref({
+    name:'lisi',
+    age:12
+})
+console.log(name.value);
+console.log(person);
+
+</script>
+
+
+<style scoped lang='scss'>
+
+</style>
